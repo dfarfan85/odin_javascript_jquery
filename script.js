92225b2c4e001152866dbdcd2d8c4a41fb20aa0c
David Farfan <David@davids-mbp.attlocal.net>
2016-08-18 21:35:34 -0700

    8-18-2016: update

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 $(document).ready(function() {
 	
-	for(var row = 0; row < 16; row++){
-		for(var column = 0; column < 16; column++){
-			$("#container").append("<div class='square'></div>");
+	 function makeGrid(gridNum){
+		for(var row = 0; row < gridNum; row++){
+			for(var column = 0; column < gridNum; column++){
+				$("#container").append("<div class='square'></div>");
+			}
 		}
+
+
+		var h = ((500 - 2*gridNum)/gridNum);
+		console.log(h);
+		$(".square").css('width', (500 - 2*gridNum)/gridNum);
+		$(".square").css('height', (500 - 2*gridNum)/gridNum);
+
+
+		$('.square').hover(
+			function(){
+				$(this).css('background-color', 'red');
+			},
+			function(){
+				$(this).css('background-color', 'red');
+			}
+		);
+
 	}
 
+	makeGrid(16);
+
+	
 	$('.square').hover(
 		function(){
 			$(this).css('background-color', 'red');
@@ -15,8 +37,16 @@ $(document).ready(function() {
 		}
 	);
 
+	var input = 0;
+
 	$('button').click(function(){
 		$('.square').css('background-color', 'none');
-		prompt("Please select how may squares to ");
+		input = prompt("Please select how may squares per side " + 
+			"to make the new grid?");	
+		console.log("inside:  " + input);
+		$(".square").replaceWith(makeGrid(input));
 	});
+	
+	makeGrid(input);
+	console.log("outside: " + input);
 });
