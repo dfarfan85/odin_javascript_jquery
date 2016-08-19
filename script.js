975d9554c335ee49ec12d85d1cae0e9388f946fb
David Farfan <David@davids-mbp.attlocal.net>
2016-08-18 22:13:50 -0700

    added ability to change color of squares

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 	
+	 makeGrid(16);
+	 numSquare();
+	 changeColor();
+
 	 function makeGrid(gridNum){
 		for(var row = 0; row < gridNum; row++){
 			for(var column = 0; column < gridNum; column++){
@@ -7,13 +11,14 @@ $(document).ready(function() {
 			}
 		}
 
-
-		var h = ((500 - 2*gridNum)/gridNum);
-		console.log(h);
 		$(".square").css('width', (500 - 2*gridNum)/gridNum);
 		$(".square").css('height', (500 - 2*gridNum)/gridNum);
 
+		addHover();
+	}
 
+
+	function addHover(){
 		$('.square').hover(
 			function(){
 				$(this).css('background-color', 'red');
@@ -22,31 +27,37 @@ $(document).ready(function() {
 				$(this).css('background-color', 'red');
 			}
 		);
-
+}
+	
+	function numSquare(){	
+		$('.butt-squares').click(function(){
+			$('.square').css('background-color', 'none');
+			input = prompt("Please select how may squares per side " + 
+				"to make the new grid?");	
+			$(".square").replaceWith(makeGrid(input));
+		});
 	}
-
-	makeGrid(16);
-
-	
-	$('.square').hover(
-		function(){
-			$(this).css('background-color', 'red');
-		},
-		function(){
-			$(this).css('background-color', 'red');
-		}
-	);
-
-	var input = 0;
-
-	$('button').click(function(){
-		$('.square').css('background-color', 'none');
-		input = prompt("Please select how may squares per side " + 
-			"to make the new grid?");	
-		console.log("inside:  " + input);
-		$(".square").replaceWith(makeGrid(input));
-	});
-	
-	makeGrid(input);
-	console.log("outside: " + input);
+		
+	function changeColor(color){
+		$('.butt-color').click(function() {
+			input = prompt("Choose by typing either: blue, green, yellow, " + 
+				"or gray ");
+			switch(input){
+				case 'blue':
+					$('.square').css('background-color', input)
+					break;
+				case 'green':
+					$('.square').css('background-color', input)
+					break;
+				case 'yellow':
+					$('.square').css('background-color', input)
+					break;
+				case 'gray':
+					$('.square').css('background-color', input)
+					break;
+				default:
+					changeColor();
+			}
+		});
+	}
 });
